b1df0d94f9905adbbaa6080c44d8013b2e6a275b
Yuriev Yuriy <yuriev1986@gmail.com>
2021-04-17 17:28:51 +0300

    add loader on main page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { AppBar } from './components/AppBar';
 import { authOperations, authSelectors } from './redux/auth';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import s from './view/ViewStyles.module.css';
 
 const MainView = lazy(() => import('./view/MainView'));
 const RegisterView = lazy(() => import('./view/RegisterView'));
@@ -24,7 +27,18 @@ const App = () => {
     !isReloaded && (
       <>
         <AppBar />
-        <Suspense fallback={<p>Loading...</p>}>
+        <Suspense
+          fallback={
+            <Loader
+              className={s.loader}
+              type="TailSpin"
+              color="#00BFFF"
+              height={100}
+              width={100}
+              timeout={3000} //3 secs
+            />
+          }
+        >
           <Switch>
             <PublicRoute exact path="/">
               <MainView />
